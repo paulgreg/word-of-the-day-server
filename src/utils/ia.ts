73fe78962448d4acd582ca019662b89d34f1dc3d
Grégory Paul <me@gpaul.me>
2026-01-03 23:23:32 +0100

    chore: use llm.js to be LLM agnostic

--- src/utils/ia.ts
+++ src/utils/ia.ts
@@ -1,31 +1,24 @@
-import OpenAI from 'openai'
+import LLM, { Response } from '@themaximalist/llm.js'
 import type { WordRecordType } from '../types'
 
 export const translate = async (w: string, word: string, dateStr: string): Promise<WordRecordType | undefined> => {
     try {
         console.info('translate', w)
-        const openai = new OpenAI()
-        const completion = await openai.chat.completions.create({
-            model: 'gpt-4.1',
-            store: true,
-            messages: [
-                {
-                    role: 'developer',
-                    content:
-                        'You are a translator that translate a word of the day in different languages in that order : French, Spanish, Italian and Portuguese. You should only respond with one answer per language per line, without the language, without explanation. All translations should have the same meaning.',
-                },
-                {
-                    role: 'user',
-                    content: `translate: ${w}`,
-                },
-            ],
+        const llm = new LLM({
+            service: 'openai',
+            model: 'gpt-4',
+            extended: true,
         })
+        llm.system(
+            'You are a translator that translate a word of the day in different languages in that order : French, Spanish, Italian and Portuguese. You should only respond with one answer per language per line, without the language, without explanation. All translations should have the same meaning.'
+        )
+        const response = (await llm.chat(`translate: ${w}`)) as Response
 
-        if (completion.choices.length === 0) throw new Error('no result')
-        const answer = completion.choices[0]
-        if (!answer?.message?.content) throw new Error('No answer')
+        if (!response.content) throw new Error('no result')
 
-        const translations = answer.message.content.split('\n').map((w) => w.trim())
+        console.debug(response.usage)
+
+        const translations = response.content.split('\n').map((w) => w.trim())
         console.info(`Answer:`, translations)
 
         if (translations.length != 4) throw new Error('No 4 answers')
