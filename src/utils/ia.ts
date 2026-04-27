2ceef3c4e8a1c44cbce66a5dd2a044eb5de734ef
Grégory Paul <me@gpaul.me>
2026-04-27 15:36:08 +0200

    feat: use zod and script json output

--- src/utils/ia.ts
+++ src/utils/ia.ts
@@ -1,10 +1,10 @@
 import type { WordRecordType } from '../types.js'
-import { generateText } from 'ai'
-import { createMistral } from '@ai-sdk/mistral'
+import { createMistral, type MistralLanguageModelOptions } from '@ai-sdk/mistral'
+import { generateText, Output } from 'ai'
+import { z } from 'zod'
 
-const system = `You are a translator that translate a word of the day in different languages in that order : French, Spanish, Italian and Portuguese.
-You should only respond with one answer per language per line, without the language, without explanation. Be concise.
-All translations should have the same meaning and be in lower case.`
+const system = `You are a translator that translate a word of the day in different languages : fr/French, es/Spanish, it/Italian, pt/Portuguese.
+Be concise. All translations should have the same meaning and be in lower case.`
 
 export const translate = async (w: string, word: string, dateStr: string): Promise<WordRecordType | undefined> => {
     try {
@@ -15,6 +15,19 @@ export const translate = async (w: string, word: string, dateStr: string): Promi
         })
         const response = await generateText({
             model: mistral(process.env.LLM_MODEL ?? ''),
+            providerOptions: {
+                mistral: {
+                    strictJsonSchema: true,
+                } satisfies MistralLanguageModelOptions,
+            },
+            output: Output.object({
+                schema: z.object({
+                    fr: z.string(),
+                    es: z.string(),
+                    it: z.string(),
+                    pt: z.string(),
+                }),
+            }),
             system,
             prompt: `translate: ${w}`,
         })
@@ -23,19 +36,14 @@ export const translate = async (w: string, word: string, dateStr: string): Promi
 
         console.debug(response.usage)
 
-        const translations = response.text.split('\n').map((w) => w.trim())
+        const translations = response.output
         console.info(`Answer:`, translations)
-
-        if (translations.length != 4) throw new Error('No 4 answers')
 
         return {
             w,
             word,
             date: dateStr,
-            fr: translations[0],
-            es: translations[1],
-            it: translations[2],
-            pt: translations[3],
+            ...translations,
         }
     } catch (e) {
         console.error(e)
